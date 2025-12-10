644924c8874eaa6bc7c5bbedd75acc9b785bd741
Lunar-Dawn <lunarrequiem42@gmail.com>
2025-12-10 17:42:21 +0100

    Zero-pad the module numbers so auto-sorting sorts nicer.

---
+++ src/day10.rs
@@ -0,0 +1,13 @@
+use std::path::PathBuf;
+
+use crate::util;
+
+#[allow(unused_variables)]
+pub fn part1(path: &PathBuf) -> util::Result<String> {
+    Ok("Not implemented yet!".to_string())
+}
+
+#[allow(unused_variables)]
+pub fn part2(path: &PathBuf) -> util::Result<String> {
+    Ok("Not implemented yet!".to_string())
+}
