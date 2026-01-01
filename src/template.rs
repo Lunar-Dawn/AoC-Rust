7272cc4fee0a8e61a30ca36661857094c17ae33e
Lunar-Dawn <lunarrequiem42@gmail.com>
2026-01-02 00:58:34 +0100

    Add a new template

---
+++ src/template.rs
@@ -0,0 +1,16 @@
+use crate::runner;
+use crate::util::DynResult;
+
+runner!();
+
+type ParsedData = ();
+fn parse(input: &str) -> DynResult<ParsedData> {
+    Ok(())
+}
+
+fn part1(data: &ParsedData) -> u64 {
+    0
+}
+fn part2(data: &ParsedData) -> u64 {
+    0
+}
