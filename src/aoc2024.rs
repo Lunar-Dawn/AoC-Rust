fd09ae3d7bb8471f485dae707c18a3a7389b1896
Lunar-Dawn <lunarrequiem42@gmail.com>
2026-01-05 18:07:58 +0100

    2024 day 8

--- src/aoc2024.rs
+++ src/aoc2024.rs
@@ -1,3 +1,3 @@
 use crate::year;
 
-year!(day01, day02, day03, day04, day05, day06, day07);
+year!(day01, day02, day03, day04, day05, day06, day07, day08);
---
+++ src/aoc2024/day08.rs
@@ -0,0 +1,83 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::runner;
+use crate::util::grid::{Grid, VectorGrid};
+use crate::util::point2i::Point2i;
+use crate::util::DynResult;
+
+runner!();
+
+struct Map {
+    grid: VectorGrid<char>,
+    antennae: HashMap<char, Vec<Point2i>>,
+}
+type ParsedData = Map;
+fn parse(input: &str) -> DynResult<ParsedData> {
+    let lines: Vec<_> = input.lines().collect();
+
+    let width = lines[0].len();
+    let height = lines.len();
+    let data = lines.iter().flat_map(|l| l.chars()).collect();
+
+    let grid = VectorGrid::from(width, height, data);
+
+    let mut antennae = HashMap::new();
+    for p in grid.pos_iter() {
+        let c = grid.get(&p).unwrap();
+
+        if *c == '.' {
+            continue;
+        }
+
+        antennae.entry(*c).or_insert_with(Vec::new).push(p);
+    }
+
+    Ok(Map { grid, antennae })
+}
+
+fn part1(map: &ParsedData) -> usize {
+    let mut antinodes = HashSet::new();
+
+    for (_, antennae) in map.antennae.iter() {
+        for from in antennae {
+            for to in antennae {
+                if from == to {
+                    continue;
+                }
+
+                let antinode = from + to.vec_to(from);
+                if map.grid.in_bounds(&antinode) {
+                    antinodes.insert(antinode);
+                }
+            }
+        }
+    }
+
+    antinodes.len()
+}
+fn part2(map: &ParsedData) -> usize {
+    let mut antinodes = HashSet::new();
+
+    for (_, antennae) in map.antennae.iter() {
+        for from in antennae {
+            for to in antennae {
+                if from == to {
+                    continue;
+                }
+
+                let displacement = from.vec_to(to);
+                for i in 1.. {
+                    let antinode = from + displacement * i;
+
+                    if !map.grid.in_bounds(&antinode) {
+                        break;
+                    }
+
+                    antinodes.insert(antinode);
+                }
+            }
+        }
+    }
+
+    antinodes.len()
+}
